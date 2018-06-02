4ad22b3598abf520b4b7da06e8b4d450d1cfc4b4
ivansenitsev <ivansenitsev@gmail.com>
2018-06-02 08:00:15 +0300

    ver0.1
    Yes

---
+++ ContactBookApp/ContactBookApp.Android/obj/Debug/android/src/md5d7ade476cdda8f21c72dd59956bb438b/MainActivity.java
@@ -0,0 +1,47 @@
+package md5d7ade476cdda8f21c72dd59956bb438b;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("ContactBookApp.Droid.MainActivity, ContactBookApp.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("ContactBookApp.Droid.MainActivity, ContactBookApp.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
