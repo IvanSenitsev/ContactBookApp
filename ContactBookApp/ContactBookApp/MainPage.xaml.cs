d7ac18c7c367fe16a0ce3a080c070fe8a9fe1369
ivansenitsev <ivansenitsev@gmail.com>
2018-06-02 08:55:48 +0300

    ver0.2
    Yes

--- ContactBookApp/ContactBookApp/MainPage.xaml.cs
+++ ContactBookApp/ContactBookApp/MainPage.xaml.cs
@@ -4,14 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using ContactBookApp;
+using System.Collections.ObjectModel;
+using ContactBookApp.Models;
 
 namespace ContactBookApp
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        private ObservableCollection<Models.Contact> _contacts;
+        public MainPage()
 		{
-			InitializeComponent();
-		}
-	}
+           
+            InitializeComponent();
+            _contacts = new ObservableCollection<Contact>
+            {
+                new Contact{firstName="Anna",lastName="Popova",id=1,phone="+372687865",email="anna44@mail.ru"},
+                new Contact{firstName="Peter",lastName="Black",id=1,phone="+372830012",email="straight@qiq.ru"},
+                new Contact{firstName="Rassel",lastName="Strong",id=1,phone="+372118494",email="rasselm@gmail.com"}
+            };
+
+            listView.ItemsSource = _contacts;
+        }
+
+        private void Call_Clicked(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            //через XAML свойство мы узнаём выбраный элемент
+            var contact = menuItem.CommandParameter as Contact;
+            DisplayAlert("Call", contact.firstName, "Ok");
+        }
+
+        private void Delete_Clicked(object sender, EventArgs e)
+        {
+            var contact = (sender as MenuItem).CommandParameter as Contact;
+            _contacts.Remove(contact);
+        }
+    }
 }
---
+++ ContactBookApp/ContactBookApp/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactBookApp.Models
+{
+    class Contact
+    {
+        //нужен для выяснения новый или старый контакт 0 новый 1 старый
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+
+    }
+}
