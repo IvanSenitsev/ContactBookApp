4e905647f109f89568d4c8eac67d6402b87cdfe3
ivansenitsev <ivansenitsev@gmail.com>
2018-06-02 10:20:49 +0300

    ver.05
    Yes

--- ContactBookApp/ContactBookApp/ContactDetailPage.xaml.cs
+++ ContactBookApp/ContactBookApp/ContactDetailPage.xaml.cs
@@ -12,14 +12,46 @@ namespace ContactBookApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ContactDetailPage : ContentPage
 	{
-		public ContactDetailPage (Contact contact)
+        private Contact contact;
+		public ContactDetailPage (Contact _contact)
 		{
 			InitializeComponent ();
+            contact = _contact;
             entFirstName.Text = contact.firstName;
             entLastName.Text = contact.lastName;
             entPhone.Text = contact.phone;
             entEmail.Text = contact.email;
             
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (entFirstName.Text == "")
+            {
+                DisplayAlert("Attention", "Please enter the name.","OK");
+            }
+            else if (entLastName.Text == "")
+            {
+                DisplayAlert("Attention", "Please enter the last name.", "OK");
+            }
+            else if (entPhone.Text == "")
+            {
+                DisplayAlert("Attention", "Please enter phone number.", "OK");
+            }
+            else if (entEmail.Text == "")
+            {
+                DisplayAlert("Attention", "Please enter email.", "OK");
+            }
+            else if(contact.id==0)
+            {
+                DisplayAlert("Attention", "New Contact Added", "OK");
+                //добовляем новый контакт
+            }
+            else if (contact.id == 1)
+            {
+                DisplayAlert("Attention", "Contact Changet", "OK");
+                //добовляем новый контакт
+            }
+        }
+    }
 }
